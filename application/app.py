755c4b20110510f7401e721d4f52160509031ec0
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-12-28 06:57:47 +0600

    Initial Setup

--- application/app.py
+++ application/app.py
@@ -1,1 +1,16 @@
 from flask import Flask
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+
+app = Flask(__name__)
+api=Api(app)
+
+client = MongoClient("mongodb://database:27017")
+db = client.Th
+collection = db["Collection"]
+
+class Test(Resource):
+    pass
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
