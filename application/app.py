fbd557d408b14bd97228ff61df44bd07fd3488a1
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-12-28 09:40:59 +0600

    Post method

--- application/app.py
+++ application/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 
@@ -10,7 +10,35 @@ db = client.Th
 collection = db["Collection"]
 
 class Test(Resource):
-    pass
+    def post(self):
+        postedData = request.get_json()
+
+        if "key" not in postedData or "value" not in postedData:
+            retJson = {
+                "status" : 301,
+                "message" : "Missing key or value!!!!!!!"
+            }
+
+            return jsonify(retJson)
+
+        key = postedData["key"]
+        value = postedData["value"]
+
+        collection.insert({
+            "key" : key,
+            "value" : value
+        })
+
+        retJson = {
+            "status" : 200,
+            "message" : "Successfully inserted!!!!!!!!"
+        }
+
+        return jsonify(retJson)
+
+
+api.add_resource(Test, '/values')
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
